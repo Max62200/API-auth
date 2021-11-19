d50e905f4644fca18e5898780a0443b54c51de24
Sebastien-Delansay <62352138+Sebastien-Delansay@users.noreply.github.com>
2021-11-19 13:39:32 +0100

    back first try finish

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,5 +11,9 @@ router.post('/users/register/', userController.register);
 
 router.post('/users/login/', userController.login);
 
+router.get('/users/me/', userController.getUserProfile);
+
+router.put('/users/me/', userController.updateUserProfile);
+
 // On exporte le router
 module.exports = router;
